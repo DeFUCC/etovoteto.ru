a95093af0ad3a75dd31bf8eb74fa9fd35390fb94
davay42 <fond@fondfrukt.ru>
2021-05-26 15:12:16 +0300

    gh-pages

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+cd build
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:etovoteto/etovoteto.ru.git master:gh-pages
+
+cd -
--- site/_theme/_scripts/main.js
+++ site/_theme/_scripts/main.js
@@ -1,6 +1,26 @@
 import VanillaScrollSpy from 'vanillajs-scrollspy'
 import { generateWords } from './use/randomWords'
 
+//<![CDATA[
+var owa_baseUrl = 'http://stats.frkt.ru/'
+var owa_cmds = owa_cmds || []
+owa_cmds.push(['setSiteId', '8fa0a5261e72031dfa4fe8e24981255e'])
+owa_cmds.push(['trackPageView'])
+owa_cmds.push(['trackClicks'])
+;(function () {
+  var _owa = document.createElement('script')
+  _owa.type = 'text/javascript'
+  _owa.async = true
+  owa_baseUrl =
+    'https:' == document.location.protocol
+      ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:')
+      : owa_baseUrl
+  _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js'
+  var _owa_s = document.getElementsByTagName('script')[0]
+  _owa_s.parentNode.insertBefore(_owa, _owa_s)
+})()
+//]]>
+
 document.addEventListener('DOMContentLoaded', () => {
   scrollSpy()
   generator()
